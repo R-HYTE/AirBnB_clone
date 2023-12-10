f826d9b96501a53ef73208e385cde08fa76f57b4
R_HYTE <polycarpwathuta39@gmail.com>
2023-12-10 22:17:47 +0300

    Added docstrings

--- models/user.py
+++ models/user.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
+'''
+This module defines the User class, which inherits from the BaseModel class.
+'''
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    '''
+
+    User class that inherits from BaseModel.
+    Attributes:
+        email (str): User's email.
+        password (str): User's password.
+        first_name (str): User's first name.
+        last_name (str): User's last name.
+    '''
+
     email = ""
     password = ""
     first_name = ""
