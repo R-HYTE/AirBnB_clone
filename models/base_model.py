8b5da34630305d8f18706b13abdc0df74ffaaffe
R_HYTE <polycarpwathuta39@gmail.com>
2023-12-10 21:24:22 +0300

    Fixed to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -37,6 +37,15 @@ class BaseModel:
         # Add the new instance to the storage
         models.storage.new(self)
 
+    def __str__(self):
+        '''
+        Return the string representation of the BaseModel instance.
+
+        Returns:
+            str: String representation of the instance.
+        '''
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
         '''
         Updates `updated_at` with the current datetime and save the instance.
@@ -51,18 +60,13 @@ class BaseModel:
         Returns:
             dict: Dictionary containing all keys/values of the instance.
         '''
-        return {
-            "id": self.id,
-            "created_at": self.created_at.isoformat(),
-            "updated_at": self.updated_at.isoformat(),
-            "__class__": self.__class__.__name__,
-        }
+        modified_instance_dict = self.__dict__.copy()
 
-    def __str__(self):
-        '''
-        Return the string representation of the BaseModel instance.
+        # Add __class__ key with the class name of the object
+        modified_instance_dict['__class__'] = self.__class__.__name__
 
-        Returns:
-            str: String representation of the instance.
-        '''
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        # Convert created_at and updated_at to string in ISO format
+        modified_instance_dict['created_at'] = self.created_at.isoformat()
+        modified_instance_dict['updated_at'] = self.updated_at.isoformat()
+
+        return modified_instance_dict
