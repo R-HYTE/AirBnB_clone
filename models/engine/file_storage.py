89504c6071411bf06b09e6dd22f39a167d011a19
R_HYTE <polycarpwathuta39@gmail.com>
2023-12-10 22:09:01 +0300

    Updated filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -50,7 +50,7 @@ class FileStorage:
         Args:
             obj: The instance to be added.
         '''
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
 
     def save(self):
